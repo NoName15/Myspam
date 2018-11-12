71a90af7c1ac1b6fdebb8b009a8ec8cfce83b40f
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-11-12 21:48:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,48 @@
-var ServerID = "511633362331631621"; //اي دي السيرفر
-var ChannelID = "511639593511026698";// اي دي الروم
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('warn', console.warn);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on('error', console.error);
-
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
+client.on('ready', async() => {
+var server = "511633362331631621"; // ايدي السررفر
+var channel = "511639593511026698";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('** فرعونفؤعونفؤعونفؤعونفؤعونفؤعونفرعونفؤعونفؤعونفؤعونفؤعونفرعونفرعونفرعونفؤعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفؤعونفؤعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفؤعونفرعونفرعونفؤعونفرعونفرعونفؤعونفؤعونفؤعونفؤعونفؤعونفؤعونفرعونفرعونفرعونفرعونفرعونفؤعونفرعونفرعونفرعومفرعون **')
+    },305);
+})
 
 
-    });
+
+client.on('ready', async() => {
+var server = "511633362331631621"; // ايدي السررفر
+var channel = "511639593511026698";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },8.64e+7);
+})
+
+client.on('ready', async() => {
+var server = "511633362331631621"; // ايدي السررفر
+var channel = "511639593511026698";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
+    },8.64e+7);
+})
+
+
+client.on('message', message => {
+if(message.content.startsWith('1s')) {
+if(message.author.id !== "480407581085532180") return;
+var args = message.content.split(' ').slice(1).join(' ');
+message.channel.send(args);
 }
-
-
-const devs = ['480407581085532180'];
-
-
- client.on('message', message => {
-var prefix = "."
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`${prefix}say <words>`);
-
-if (command == "say") {
-if(!message.guild.member(message.author).hasPermission("ADMINSTRATOR")) return message.reply("*لا تملك الصلاحيات المطلوبه**");
-
-message.channel.send(args.join("  "))
-    message.delete();
-  }
-
-
-
-}); 
+});
+ 
 
 
 
 
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN); 
